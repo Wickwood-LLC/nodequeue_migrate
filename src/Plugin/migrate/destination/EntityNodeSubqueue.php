aab8e45f1b72aeb536735b3c38dba09e1e04d6fd
Jo Fitzgerald <jo.fitzgerald@computerminds.co.uk>
2016-07-18 11:09:03 +0100

    Initial commit.

---
+++ src/Plugin/migrate/destination/EntityNodeSubqueue.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Drupal\nodequeue_migrate\Plugin\migrate\destination;
+
+use Drupal\migrate\Plugin\migrate\destination\EntityContentBase;
+
+/**
+ * @MigrateDestination(
+ *   id = "entity:entity_subqueue"
+ * )
+ */
+class EntityNodeSubqueue extends EntityContentBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getIds() {
+    $id_key = $this->getKey('id');
+    $ids[$id_key]['type'] = 'string';
+    return $ids;
+  }
+
+}
